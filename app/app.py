62da3bf66a548ee0b1a80b04ca774b45e0eeff9a
James Hasty <jameshasty22@gmail.com>
2020-10-13 21:02:00 -0400

    final working app

--- app/app.py
+++ app/app.py
@@ -1,20 +1,47 @@
 # Import Dependencies 
-from flask import Flask, render_template, redirect
+from flask import Flask, request, render_template, redirect
 from flask_pymongo import PyMongo
 from flask_assets import Environment, Bundle
 from flask_scss import Scss
+from datetime import datetime
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
 import os
 
 
 
 # Create an instance of Flask app
 app = Flask(__name__)
-assets_folder = os.path.join(app.root_path, 'assets')
 
 @app.route("/")
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/Stock_Select', methods=['POST']) 
+def Stock_Select(): 
+   
+    stock = request.form.get('symb') 
+    stockstdate = request.form.get('start')
+    stockedate = request.form.get('end')
+
+
+    stockv = yf.Ticker(stock)
+    sdate = (stockstdate)
+    edate = (stockedate)
+
+    data_df = yf.download(stock, start=sdate, end=edate)
+
+    data_df.index = [x for x in range(1, len(data_df.values)+1)]
+    data_df.index.name = 'id'
+    data_df.index = data_df.index.map(str)
+
+    data_df.to_csv('../data/external/StockETL.csv', index = False)
+
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug = True)
+
+

b2341448ecc881513f13e2e66a3e94488d5a03c4
James Hasty <jameshasty22@gmail.com>
2020-10-16 08:29:37 -0400

    JH changes

---
+++ app/static/js/legend.js
@@ -0,0 +1,21 @@
+  
+const legend = () => {
+    const labelJoin = fc.dataJoin("text", "legend-label");
+    const valueJoin = fc.dataJoin("text", "legend-value");
+  
+    const instance = selection => {
+      selection.each((data, selectionIndex, nodes) => {
+        labelJoin(d3.select(nodes[selectionIndex]), data)
+          .attr("transform", (_, i) => "translate(50, " + (i + 1) * 15 + ")")
+          .text(d => d.name);
+  
+        valueJoin(d3.select(nodes[selectionIndex]), data)
+          .attr("transform", (_, i) => "translate(60, " + (i + 1) * 15 + ")")
+          .text(d => d.value);
+      });
+    };
+  
+    instance.xScale = () => instance;
+    instance.yScale = () => instance;
+    return instance;
+  };
